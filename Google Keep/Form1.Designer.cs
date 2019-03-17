02a1eac31f16c7e04897772600611ce44c228eec
Agha Shayan <39364365+AghaShayan123@users.noreply.github.com>
2019-03-17 14:42:19 +0500

    Final

--- Google Keep/Form1.Designer.cs
+++ Google Keep/Form1.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // webBrowser1
@@ -38,21 +40,24 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(429, 370);
+            this.webBrowser1.Size = new System.Drawing.Size(350, 309);
             this.webBrowser1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 370);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(350, 309);
             this.Controls.Add(this.webBrowser1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Google Keep";
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -61,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Google Keep/Form1.cs
+++ Google Keep/Form1.cs
@@ -16,6 +16,7 @@ namespace Google_Keep
     {
         public Form1()
         {
+//            timer1.Start();
             RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
             reg.SetValue("Google Keep", Application.ExecutablePath.ToString());
             InitializeComponent();
@@ -23,15 +24,19 @@ namespace Google_Keep
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int x = Screen.PrimaryScreen.WorkingArea.Right - this.Width;
+            this.Location = new Point(x, 0);
+            this.ShowInTaskbar = false;
             try
             {
                 StringBuilder queryaddress = new StringBuilder();
-                queryaddress.Append("https://keep.google.com/u/0/");
+                queryaddress.Append("https://keep.google.com");
                 webBrowser1.Navigate(queryaddress.ToString());
+                webBrowser1.ScriptErrorsSuppressed = true;
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message.ToString(), "Error");
+
             }
             
         }
